ae1192604496194b6f777e27b5ec12d84db44aa7
Thanmay1609 <ntmay1609@gmail.com>
2026-02-28 12:05:47 +0530

    UC5: Render OOPS as Banner using Array Initialization

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,25 +1,24 @@
 /**
  * @author Thanmay
- * @version 4.0
+ * @version 5.0
  */
 public class OOPSBannerApp {
     public static void main(String[] args) {
         
-        // An array that stores all 7 lines
-        String[] bannerLines = new String[7];
         
-        // Fill the array with each line
-        bannerLines[0] = String.join("  ", " OOO ", " OOO ", "PPPP ", "SSS  ");
-        bannerLines[1] = String.join("  ", "O   O", "O   O", "P   P", "S    ");
-        bannerLines[2] = String.join("  ", "O   O", "O   O", "P   P", "S    ");
-        bannerLines[3] = String.join("  ", "O   O", "O   O", "PPPP ", " SS  ");
-        bannerLines[4] = String.join("  ", "O   O", "O   O", "P    ", "   S ");
-        bannerLines[5] = String.join("  ", "O   O", "O   O", "P    ", "   S ");
-        bannerLines[6] = String.join("  ", " OOO ", " OOO ", "P    ", "SSS  ");
+        String[] bannerLines = {
+            String.join("  ", " OOO ", " OOO ", "PPPP ", "SSS  "),
+            String.join("  ", "O   O", "O   O", "P   P", "S    "),
+            String.join("  ", "O   O", "O   O", "P   P", "S    "),
+            String.join("  ", "O   O", "O   O", "PPPP ", " SS  "),
+            String.join("  ", "O   O", "O   O", "P    ", "   S "),
+            String.join("  ", "O   O", "O   O", "P    ", "   S "),
+            String.join("  ", " OOO ", " OOO ", "P    ", "SSS  ")
+        };
         
-        // A loop to print all lines
-        for (int i = 0; i < bannerLines.length; i++) {
-            System.out.println(bannerLines[i]);
+        
+        for (String line : bannerLines) {
+            System.out.println(line);
         }
         
     }
