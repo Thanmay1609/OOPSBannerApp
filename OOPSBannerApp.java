e33776dcbdcadd5b5502d288e2dd9d6d06d4e004
Thanmay1609 <ntmay1609@gmail.com>
2026-03-03 09:34:56 +0530

    UC6: Render OOPS Banner using Static Helper Methods

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,21 +1,51 @@
 /**
+ * OOPS Banner App - UC6 - Static Functions
  * @author Thanmay
- * @version 5.0
+ * @version 6.0
  */
 public class OOPSBannerApp {
+    
+    public static String buildLine1() {
+        return String.join("  ", " OOO ", " OOO ", "PPPP ", "SSS  ");
+    }
+    
+    
+    public static String buildLine2() {
+        return String.join("  ", "O   O", "O   O", "P   P", "S    ");
+    }
+    
+    
+    public static String buildLine3() {
+        return String.join("  ", "O   O", "O   O", "P   P", "S    ");
+    }
+    
+    public static String buildLine4() {
+        return String.join("  ", "O   O", "O   O", "PPPP ", " SS  ");
+    }
+    
+    public static String buildLine5() {
+        return String.join("  ", "O   O", "O   O", "P    ", "   S ");
+    }
+    
+    public static String buildLine6() {
+        return String.join("  ", "O   O", "O   O", "P    ", "   S ");
+    }
+    
+    public static String buildLine7() {
+        return String.join("  ", " OOO ", " OOO ", "P    ", "SSS  ");
+    }
+    
     public static void main(String[] args) {
         
-        
         String[] bannerLines = {
-            String.join("  ", " OOO ", " OOO ", "PPPP ", "SSS  "),
-            String.join("  ", "O   O", "O   O", "P   P", "S    "),
-            String.join("  ", "O   O", "O   O", "P   P", "S    "),
-            String.join("  ", "O   O", "O   O", "PPPP ", " SS  "),
-            String.join("  ", "O   O", "O   O", "P    ", "   S "),
-            String.join("  ", "O   O", "O   O", "P    ", "   S "),
-            String.join("  ", " OOO ", " OOO ", "P    ", "SSS  ")
+            buildLine1(),
+            buildLine2(),
+            buildLine3(),
+            buildLine4(),
+            buildLine5(),
+            buildLine6(),
+            buildLine7()
         };
-        
         
         for (String line : bannerLines) {
             System.out.println(line);
